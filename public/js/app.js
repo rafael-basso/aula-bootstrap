6b1fdd613fd4731e0a552a35f7ca9ed436f584cb
Rafael <rafael_basso81@hotmail.com>
2022-02-07 10:54:19 -0300

    if-responseText

--- public/js/app.js
+++ public/js/app.js
@@ -1,48 +1,39 @@
 $(function () {
-  $(".toaster").on("click", function () {
-    let inputEmail = $('#inputEmail').val()
-    let msg = $('#msg').val()
+    $(".toaster").on("click", function () {
+        let inputEmail = $("#inputEmail").val();
+        let msg = $("#msg").val();
 
-        
-    toastr.options.positionClass = "toast-bottom-right";
+        toastr.options.positionClass = "toast-bottom-right";
 
-    
         if (msg == "" || inputEmail == "") {
-          toastr.clear()
-          toastr.error('Please type your email and message')
-        } else {
-          
-          let data = {
-            inputEmail: inputEmail,
-            msg: msg
-          }
-          
-            let xhr = new XMLHttpRequest();
-            xhr.open('POST', '/');
-            xhr.setRequestHeader('content-type', 'application/json')
-            // xhr.onload = function () {
-            //   console.log(xhr.responseText)
-            //   if(xhr.responseText == 'success') {
-            //     alert('Email sent)
-            //     msg.value = "";
-            //     inputEmail.value = ""
-            //   } else {
-            //     alert('Bad response. Email not sent)
-            //   }
-            // }
-            
             toastr.clear();
-            toastr.success('Email sent successfully!')
-            $('#msg').val('')
-            $('#inputEmail').val('')            
+            toastr.error("Please type your email and message");
+        } else {
+            let data = {
+                inputEmail: inputEmail,
+                msg: msg,
+            };
 
-            xhr.send(JSON.stringify(data))
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", "/");
+            xhr.setRequestHeader("content-type", "application/json");
+            xhr.onload = function () {
+                // console.log(xhr.responseText);
+                if (xhr.responseText) {
+                    toastr.clear();
+                    toastr.success("Email sent successfully!");
+                    $("#msg").val("");
+                    $("#inputEmail").val("");
+                } else {
+                  toastr.clear();
+                  toastr.error("Bad response! Pleasae try again later.");
+                }
+            };
+
+            xhr.send(JSON.stringify(data));
         }
-        
-        
     });
 });
-
 
 function upperCase(element) {
     element.style.fontSize = "25px";
